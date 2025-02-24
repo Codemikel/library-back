5972f049724c21bd6f85378ea498fe3e28ca5ba6
Codemikel <mdrssog@protonmail.com>
2025-02-24 10:46:07 -0500

    API endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\v1\BookController;
+use App\Http\Controllers\Api\v1\LoanController;
+use App\Http\Controllers\Api\v1\AuthorController;
+use App\Http\Controllers\Api\v1\GenreController;
+use App\Http\Controllers\Api\v1\UserController;
+
+
+Route::apiResource('/v1/libros', BookController::class);
+
+Route::apiResource('/v1/prestamos', LoanController::class);
+
+Route::apiResource('/v1/autores', AuthorController::class);
+
+Route::apiResource('/v1/generos', GenreController::class);
+
+Route::apiResource('/v1/usuarios', UserController::class);
