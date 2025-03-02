219ce202f35f6938a60cf1166ae5c17ec22aa4fe
Codemikel <codemikel@proton.me>
2025-03-02 13:16:44 -0500

    SoftDeletes for models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
+
+    use SoftDeletes;
+
     protected $fillable = ['name', 'author_id', 'genre_id', 'available'];
 
     /**
@@ -27,5 +32,15 @@ class Book extends Model
     public function genre(): BelongsTo
     {
         return $this->belongsTo(Genre::class);
+    }
+
+    /**
+     * Get all of the loans for the Book
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function loans(): HasMany
+    {
+        return $this->hasMany(Loan::class);
     }
 }
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
 class Loan extends Model
 {
+
+    use SoftDeletes;
+
     protected $fillable = ['book_id', 'user_id', 'loan_date', 'return_date', 'returned'];
 
     protected function updatedAt(): Attribute {
